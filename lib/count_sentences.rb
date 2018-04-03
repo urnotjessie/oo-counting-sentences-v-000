b512f52214ebb330edc2d092678a9296c19c4ac3
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-03 21:05:51 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+      self.end_with?(".")
   end
 
   def question?
-
+      self.end_with?("?")
   end
 
   def exclamation?
-
+      self.end_with?("!")
   end
 
   def count_sentences
-
+      sentences = self.split(".").
+        flat_map{ |sentences| sentences.split("!")}.
+        flat_map{ |question| question.split("?")}
+      sentences.count
   end
 end
+
+test = "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
